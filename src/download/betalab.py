dfba1eb300bb417fe64966dce0bdfb915224c1cf
Väinö Yrjänäinen <vainoyrjanainen@icloud.com>
2020-12-08 18:06:17 +0100

    Count pages

--- src/download/betalab.py
+++ src/download/betalab.py
@@ -1,27 +1,56 @@
 from kblab import Archive
 import getpass
+import numpy as np
+import pandas as pd
+import time
 
-def main():
+def login_to_archive():
     username = input("Username: ")
     password = getpass.getpass()
     print("Password set for user:", username)
     
-    a = Archive('https://betalab.kb.se', auth=(username, password))
+    return Archive('https://betalab.kb.se', auth=(username, password))
     
-    package_ids = a.search({ 'tags': 'protokoll', 'meta.created': '1985'}, max=200)
+def count_pages(start, end):
+    years = range(start, end)
+    archive = login_to_archive()
     
-    print(type(package_ids))
-    print("Found", len(package_ids), "matches.")
+    rows = []
     
-    for package_id in package_ids:
-        print("Id:", package_id)
+    now = time.time()
+    diffs = []
+    for year in years:
+        params = { 'tags': 'protokoll', 'meta.created': str(year)}
+        package_ids = archive.search(params, max=200)
         
-        package = a.get(package_id)
-        filelist = package.list()
+        for package_id in package_ids:
+            #print("Id:", package_id)
+            
+            package = archive.get(package_id)
+            filelist = package.list()
+            
+            jp2list = [f for f in filelist if f.split(".")[-1] == "jp2"]
+            page_count = len(jp2list)
+            #print("Length of jp2 file list", page_count)
+            
+            rows.append([package_id, year, page_count])
+            
+        then = now
+        now = time.time()
+        diffs.append(now - then)
         
-        jp2list = [f for f in filelist if f.split(".")[-1] == "jp2"]
-        print(jp2list, len(jp2list))
+        avg_diff = np.mean(diffs[-5:])
+        
+        print("Avg time", avg_diff, "; to go", avg_diff * (end-year - 1) )
+            
+    columns = ["package_id", "year", "pages"]
+    db_pages = pd.DataFrame(rows, columns=columns)
+    return db_pages
     
 if __name__ == "__main__":
-    main()
+    db_pages = count_pages(1920,1990)
+    
+    print(db_pages)
+    
+    db_pages.to_csv("db/protocols/pages.csv")
 
