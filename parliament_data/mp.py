126b2ad10b165c9163c9b6ccdd8ba36b00edf805
ninpnin <vainoyrjanainen@icloud.com>
2020-12-17 15:10:36 +0200

    Combine MP db creation and MP detection in the example script

--- parliament_data/mp.py
+++ parliament_data/mp.py
@@ -8,45 +8,48 @@ def create_database(csv_path):
 
     df = pd.read_csv(csv_path)
 
-    years = csv_path.split(".")[0].split("-")
+    years = csv_path.split("/")[-1].split(".")[0].split("-")
 
     start = int(years[0])
     end = int(years[1])
 
-    
-    pass
+    df["start"] = start
+    df["end"] = end
+
+    return df
 
 
 def detect_mp(introduction, metadata, mp_db):
     """
+    Detect which member of parliament is mentioned in a given introduction.
+
+    Params:
+        introuction: An introduction to a speech in the protocols. String.
+        metadata: Year and chamber of the parliamentary proceeding in question. Dict.
+        mp_db: MP database as a Pandas DataFrame.
     """
 
     year = metadata["year"]
     mp_db = mp_db[(mp_db["start"] <= year) & (mp_db["end"] >= year)]
 
     for ix, row in mp_db.iterrows():
-        name = row["name"]
+        name = row["Riksdagsledamot"]
         last_name = " ".join(name.split()[1:])
         if last_name in introduction:
             return row
 
 
 if __name__ == '__main__':
-    
-    columns = ["name", "constituency", "party", "start", "end"]
-    row1 = ["Håkan Svensson", "Stockholm", "olego", 1970, 1970]
-    row2 = ["Olof Palme", "Linköping", "olego", 1970, 1970]
 
-    mp_db = pd.DataFrame([row1, row2], columns=columns)
+    mp_db = create_database("data/mp/1971-1973.csv")
     print(mp_db)
 
     introduction = "Anf. Palme yttrade:"
 
     metadata = dict(
-        year=1970,
+        year=1972,
     )
 
     mp = detect_mp(introduction, metadata, mp_db)
 
     print("MP:", mp)
-
