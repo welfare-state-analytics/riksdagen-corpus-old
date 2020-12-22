82a64ae9b6c924503158494becdc64ad1c918da0
ninpnin <vainoyrjanainen@icloud.com>
2020-12-22 16:29:08 +0200

    Also test parla clarin generated by us

--- test/schemas.py
+++ test/schemas.py
@@ -1,19 +1,39 @@
 import unittest
 
-import math
-import os
+from lxml import etree
 from parliament_data.utils import validate_xml_schema
+from parliament_data.download.count_pages import get_blocks
+from parliament_data.segmentation import create_parlaclarin, infer_metadata
 
 class Test(unittest.TestCase):
 
-    # SGNS log probability. Batch size == 2
+    # Official example parla-clarin 
     def test_official_example(self):
-        # Reads all files from datapath.
         schema_path = "schemas/parla-clarin.xsd"
-        xml_path = "data/parla-clarin/official-example.xml"
+        parlaclarin_path = "data/parla-clarin/official-example.xml"
         
-        valid = validate_xml_schema(xml_path, schema_path)
-        # Vocab is a bijection
+        valid = validate_xml_schema(parlaclarin_path, schema_path)
+        self.assertEqual(valid, True)
+
+    # Parla-clarin generated from example OCR XML
+    def test_generated_example(self):
+        schema_path = "schemas/parla-clarin.xsd"
+        xml_path = "data/xml/prot_198990__93-031.xml"
+        
+        xml_f = open(xml_path)
+        content_blocks = get_blocks(xml_f.read())
+        xml_f.close()
+        metadata = infer_metadata(xml_path)
+        
+        parla_clarin = create_parlaclarin(content_blocks, metadata)
+        parla_clarin_str = etree.tostring(parla_clarin, pretty_print=True, encoding="utf-8").decode("utf-8")
+        
+        parlaclarin_path = "data/parla-clarin/generated-example.xml"
+        f = open(parlaclarin_path, "w")
+        f.write(parla_clarin_str)
+        f.close()
+        
+        valid = validate_xml_schema(parlaclarin_path, schema_path)
         self.assertEqual(valid, True)
 
 
