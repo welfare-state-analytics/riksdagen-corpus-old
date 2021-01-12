a89bbfa9f329c45393f2181daff65939f9070e79
ninpnin <vainoyrjanainen@icloud.com>
2021-01-12 13:01:52 +0200

    Edit pipeline

--- dl_and_gen_pc.py
+++ dl_and_gen_pc.py
@@ -1,5 +1,5 @@
 from parliament_data.download import login_to_archive
-from parliament_data.segmentation import instance_workflow, infer_metadata, download_and_convert_to_parlaclarin
+from parliament_data.segmentation import instance_workflow, infer_metadata, gen_parlaclarin_corpus
 import pandas as pd
 
 
@@ -16,28 +16,41 @@ def instances(package_ids, archive, pattern_db, mp_db):
 
     return instance_db
 
-def parlaclarin(package_ids, archive, instance_db=None):
-    for package_id in package_ids:
-        if instance_db is None:
-            current_instances = pd.read_csv("db/segmentation/instances/" + package_id + ".csv")
-        else:
-            current_instances = instance_db[instance_db["filename"] == package_id]
-
-        parla_clarin_str = download_and_convert_to_parlaclarin(package_id, archive, current_instances, str_output=True)
+def parlaclarin(file_db, archive, instance_db=None):
+    file_db_years = [1951]
+    for corpus_year in file_db_years:
+    
+        year_db = file_db[file_db["year"] == corpus_year]
+        package_ids = file_db["package_id"]
+        package_ids = list(package_ids)
+        package_ids = sorted(package_ids)[:3]
         
-        parlaclarin_path = "data/parla-clarin/" + package_id + ".xml"
+        all_current_instances = []
+        for package_id in package_ids:
+            if instance_db is None:
+                current_instances = pd.read_csv("db/segmentation/instances/" + package_id + ".csv")
+            else:
+                current_instances = instance_db[instance_db["filename"] == package_id]
+            all_current_instances.append(current_instances)
+        
+        current_instances = pd.concat(all_current_instances)
+        
+        corpus_metadata = dict(
+            document_title="Riksdagens protocols " + str(corpus_year)
+        )
+        parla_clarin_str = gen_parlaclarin_corpus(package_ids, archive, current_instances, corpus_metadata=corpus_metadata)
+        
+        parlaclarin_path = "data/parla-clarin/" + "corpus" + str(corpus_year) + ".xml"
         f = open(parlaclarin_path, "w")
         f.write(parla_clarin_str)
         f.close()
-        
-    return parla_clarin_str
 
 if __name__ == "__main__":
     
     file_db = pd.read_csv("db/protocols/files.csv")
     
-    start_year = 1921
-    end_year = 1950
+    start_year = 1951
+    end_year = 1951
     
     file_db = file_db[file_db["year"] >= start_year]
     file_db = file_db[file_db["year"] <= end_year]
@@ -50,13 +63,12 @@ if __name__ == "__main__":
     pattern_db = pd.read_json("db/segmentation/patterns.json", orient="records", lines=True)
     archive = login_to_archive()
     
-    if True:
+    if False:
         instance_db = instances(package_ids, archive, pattern_db, mp_db)
     else:
         instance_db = None
     
-    parlaclarin(package_ids, archive, instance_db = instance_db)
-    
-    
-    
-    
+    parlaclarin(file_db, archive, instance_db = instance_db)
+
+
+
