2075e5068b97fe28e6213e5f80b5eb9188c7b3f4
Väinö Yrjänäinen <vainoyrjanainen@icloud.com>
2021-02-11 15:54:50 +0100

    Test protocol schema

--- test/schemas.py
+++ test/schemas.py
@@ -18,6 +18,14 @@ class Test(unittest.TestCase):
         valid = validate_xml_schema(parlaclarin_path, schema_path)
         self.assertEqual(valid, True)
 
+    # Official example parla-clarin 
+    def test_protocol(self):
+        schema_path = "schemas/protocol.xsd"
+        protocol_path = "data/protocols/prot-198990--93/original.xml"
+        
+        valid = validate_xml_schema(protocol_path, schema_path)
+        self.assertEqual(valid, True)
+
     # Parla-clarin generated from example OCR XML
     def test_generated_example(self):
         schema_path = "schemas/parla-clarin.xsd"
@@ -31,7 +39,6 @@ class Test(unittest.TestCase):
         mp_db = pd.read_csv("db/mp/members_of_parliament.csv")
         segmentation_patterns = load_patterns(phase="segmentation")
         segmentation_db = find_instances(protocol_id, None, segmentation_patterns, mp_db)
-        print(segmentation_db[segmentation_db["segmentation"] != "metadata"])
         protocol = apply_instances(content_blocks, segmentation_db)
         tei = create_tei(protocol, metadata)
         parla_clarin_str = create_parlaclarin(tei, metadata)
