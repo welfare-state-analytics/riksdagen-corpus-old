4ac54d895c568b9b7769ebce7ac9a5b85253bc4b
Väinö Yrjänäinen <vainoyrjanainen@icloud.com>
2021-03-03 19:20:14 +0100

    Refactor

--- find_dates.py
+++ find_dates.py
@@ -7,29 +7,25 @@ import os
 import progressbar
 
 pc_folder = "corpus/"
-protocol_ids = os.listdir(pc_folder)
-protocol_ids = [protocol_id.replace(".xml", "") for protocol_id in protocol_ids if protocol_id.split(".")[-1] == "xml"]
-
 folders = os.listdir(pc_folder)
-
-mp_db = pd.read_csv("db/mp/members_of_parliament.csv")
 
 parser = etree.XMLParser(remove_blank_text=True)
 for outfolder in progressbar.progressbar(folders):
-    outfolder = outfolder + "/"
-    protocol_ids = os.listdir(pc_folder + outfolder)
-    protocol_ids = [protocol_id.replace(".xml", "") for protocol_id in protocol_ids if protocol_id.split(".")[-1] == "xml"]
+    if os.path.isdir(pc_folder + outfolder):
+        outfolder = outfolder + "/"
+        protocol_ids = os.listdir(pc_folder + outfolder)
+        protocol_ids = [protocol_id.replace(".xml", "") for protocol_id in protocol_ids if protocol_id.split(".")[-1] == "xml"]
 
-    for protocol_id in protocol_ids:
-        metadata = infer_metadata(protocol_id)
-        filename = pc_folder + outfolder + protocol_id + ".xml"
-        
-        root = etree.parse(filename, parser)
-        root, dates = detect_date(root, metadata["year"])
+        for protocol_id in protocol_ids:
+            metadata = infer_metadata(protocol_id)
+            filename = pc_folder + outfolder + protocol_id + ".xml"
+            
+            root = etree.parse(filename, parser)
+            root, dates = detect_date(root, metadata["year"])
 
-        b = etree.tostring(root, pretty_print=True, encoding="utf-8", xml_declaration=True)
+            b = etree.tostring(root, pretty_print=True, encoding="utf-8", xml_declaration=True)
 
-        f = open(filename, "wb")
-        f.write(b)
-        f.close()
-        
+            f = open(filename, "wb")
+            f.write(b)
+            f.close()
+            
--- resegment.py
+++ resegment.py
@@ -6,11 +6,6 @@ import pandas as pd
 import os
 import progressbar
 
-def get_folder(protocol_id):
-    fpath = protocol_id[5:]
-    fpath = fpath.split("-")[0]
-    return fpath + "/"
-
 pc_folder = "corpus/"
 folders = os.listdir(pc_folder)
 
@@ -18,36 +13,38 @@ mp_db = pd.read_csv("db/mp/members_of_parliament.csv")
 
 parser = etree.XMLParser(remove_blank_text=True)
 for outfolder in progressbar.progressbar(folders):
-    outfolder = outfolder + "/"
-    protocol_ids = os.listdir(pc_folder + outfolder)
-    protocol_ids = [protocol_id.replace(".xml", "") for protocol_id in protocol_ids if protocol_id.split(".")[-1] == "xml"]
+    for outfolder in progressbar.progressbar(folders):
+        if os.path.isdir(pc_folder + outfolder):
+            outfolder = outfolder + "/"
+            protocol_ids = os.listdir(pc_folder + outfolder)
+            protocol_ids = [protocol_id.replace(".xml", "") for protocol_id in protocol_ids if protocol_id.split(".")[-1] == "xml"]
 
-    for protocol_id in protocol_ids:
-        metadata = infer_metadata(protocol_id)
-        filename = pc_folder + outfolder + protocol_id + ".xml"
-        root = etree.parse(filename, parser).getroot()
+            for protocol_id in protocol_ids:
+                metadata = infer_metadata(protocol_id)
+                filename = pc_folder + outfolder + protocol_id + ".xml"
+                root = etree.parse(filename, parser).getroot()
 
-        year = metadata["year"]
-        #print(year, type(year))
-        years = [int(elem.attrib.get("when").split("-")[0]) for elem in root.findall(".//{http://www.tei-c.org/ns/1.0}docDate")]
+                year = metadata["year"]
+                #print(year, type(year))
+                years = [int(elem.attrib.get("when").split("-")[0]) for elem in root.findall(".//{http://www.tei-c.org/ns/1.0}docDate")]
 
-        if not year in years:
-            year = years[0]
+                if not year in years:
+                    year = years[0]
 
-        if str(year) not in protocol_id:
-            print(protocol_id, year)
-        year_mp_db = filter_db(mp_db, year=year)
-        names = year_mp_db["name"]
-        ids = year_mp_db["id"]
-        names_ids = list(zip(names,ids))
+                if str(year) not in protocol_id:
+                    print(protocol_id, year)
+                year_mp_db = filter_db(mp_db, year=year)
+                names = year_mp_db["name"]
+                ids = year_mp_db["id"]
+                names_ids = list(zip(names,ids))
 
-        pattern_db = load_patterns()
-        root = find_introductions(root,pattern_db,names_ids)
-        root = detect_mps(root,names_ids,pattern_db)
-        root = format_texts(root)
-        root = update_ids(root, protocol_id)
-        b = etree.tostring(root, pretty_print=True, encoding="utf-8", xml_declaration=True)
+                pattern_db = load_patterns()
+                root = find_introductions(root,pattern_db,names_ids)
+                root = detect_mps(root,names_ids,pattern_db)
+                root = format_texts(root)
+                root = update_ids(root, protocol_id)
+                b = etree.tostring(root, pretty_print=True, encoding="utf-8", xml_declaration=True)
 
-        f = open(filename, "wb")
-        f.write(b)
-        f.close()
+                f = open(filename, "wb")
+                f.write(b)
+                f.close()
